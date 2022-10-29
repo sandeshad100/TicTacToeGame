1311a52165958587e65eb27a3ad3b6942b90edb0
sandeshad100 <sandeshadhikari20076@gmail.com>
2022-10-29 07:20:31 +0545

    Clicked item position received

--- js/app.js
+++ js/app.js
@@ -1,15 +1,53 @@
 window.addEventListener("load", function (event) {
 
+    //default array
+    var arr = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+
     const gridItem = document.querySelectorAll('.grid-item');
     const gridLen = document.querySelectorAll('.grid-item').length;
     console.log(gridItem)
     var i;
 
+    // const itemNine = document.getElementById(x).innerText;
     for (i = 0; i < gridLen; i++) {
-        gridItem[i].addEventListener('click', function () {
-            // alert("addEventListener");
+
+        gridItem[i].addEventListener('click', function (e) {
             
+            var itemNo = this.id;
+            var row = itemNo.charAt(0);
+            var col = itemNo.charAt(1);
+            
+            console.log(typeof itemNo);
+            console.log("row = " + itemNo.charAt(0));
+            console.log("col = " + itemNo.charAt(1));
+
+            ticArray(itemNo);
+
         });
+    }
+
+
+    function ticArray(x) {
+
+
+        // col = x % 10;
+        // row = Math.floor(x / 10);
+
+        // console.log("Row: " + row);
+        // console.log("Col: " + col);
+        // console.log(arr);
+
+
+        //arr[];
+
+    }
+
+    function setTicArray(index) {
+
     }
 
 
